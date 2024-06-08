9eef754b6eefe328fc1f85a21ea76fe886dca621
YukoH <hrikerdisk@gmail.com>
2024-06-08 19:28:04 +0900

    [UI/homepage/homepage] -css data removed style.css to homepage.scss
    -created: resources/js/tabs.js
    -created: resources/sass/homepag.scss
    - updata : resources\views\homepage\homepage.blade.php

--- resources/views/homepage/homepage.blade.php
+++ resources/views/homepage/homepage.blade.php
@@ -1,15 +1,9 @@
 @extends('layouts.app')
 
-<link 
-  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" 
-  rel="stylesheet" 
-  integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" 
-  crossorigin="anonymous"
->
+@vite(['resources/sass/homepage.scss','resources/js/tabs.js'])     
 
 @section('content')
 <!-- Top photo Carousel -->
-
 <div 
   id="carouselExampleAutoplaying" 
   class="carousel slide h-25"
