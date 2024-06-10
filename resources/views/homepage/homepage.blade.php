7804eced258ffe3c1f0f6cc0c9e36a2bd4a7635c
YukoH <hrikerdisk@gmail.com>
2024-06-10 19:32:11 +0900

    [UI/homepage/homepage] Create Carousel on the top of homepage.
    -change : 4 images were changed to the new.
    -create : resources\sass\carousel.scss
    create : resources\views\homepage\carousel.blade.php
    -update : resources\views\homepage\homepage.blade.php

--- resources/views/homepage/homepage.blade.php
+++ resources/views/homepage/homepage.blade.php
@@ -1,81 +1,13 @@
 @extends('layouts.app')
 
-@vite(['resources/sass/homepage.scss','resources/js/tabs.js'])     
+@vite(['resources/sass/homepage.scss',
+        'resources/js/tabs.js'])
+@vite(['resources/sass/carousel.scss'])
 
 @section('content')
-<!-- Top photo Carousel -->
-<div 
-  id="carouselExampleAutoplaying" 
-  class="carousel slide h-25"
-  data-bs-ride="carousel"
->
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <a href="#">
-        <img 
-          src="/images/homepage_appetizer.png" 
-          class="d-block w-100 h-100" 
-          alt="homepage_appetizer photo"
-        >
-      </a>
-    </div>
-    <div class="carousel-item">
-     <a href="#">
-        <img 
-          src="/images/homepage_sidedish.png" 
-          class="d-block w-100 h-100" 
-          alt="homepage_sidedish photo"
-        >
-      </a>
-    </div>
-    <div class="carousel-item">
-      <a href="#">
-        <img 
-          src="/images/homepage_maindish.png" 
-          class="d-block w-100 h-100" 
-          alt="homepage_maindish photo"
-        >
-      </a>
-    </div>
-    <div class="carousel-item">
-      <a href="#">
-        <img 
-          src="/images/homepage_dessert.jpg" 
-          class="d-block w-100 h-100" 
-          alt="homepage_dessert photo"
-        >
-      </a>
-    </div>
-  </div>
-  <button 
-    class="carousel-control-prev" 
-    type="button" 
-    data-bs-target="#carouselExampleAutoplaying" 
-    data-bs-slide="prev"
-  >
-    <span 
-      class="carousel-control-prev-icon" 
-      aria-hidden="true"
-    >
-    </span>
-    <span class="visually-hidden">
-      Previous
-    </span>
-  </button>
-  <button 
-    class="carousel-control-next" 
-    type="button" 
-    data-bs-target="#carouselExampleAutoplaying" 
-    data-bs-slide="next"
-  >
-    <span 
-      class="carousel-control-next-icon" 
-      aria-hidden="true">
-    </span>
-    <span class="visually-hidden">
-      Next
-    </span>
-  </button>
+<!-- Top image Carousel -->
+<div class="w-100 h-25">
+  @include('homepage.carousel')
 </div>
 
 <!-- tab menu area -->
