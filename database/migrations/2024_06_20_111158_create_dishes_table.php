b6fbed4d4651e4b14abacfc455e9dc3c15d7b5f7
YukoH <hrikerdisk@gmail.com>
2024-06-21 00:22:01 +0900

    [Backend/migration/posts_table] Changed strings length at name column
    -Changed : text to strings and added max length at name column

--- database/migrations/2024_06_20_111158_create_dishes_table.php
+++ database/migrations/2024_06_20_111158_create_dishes_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('dishes', function (Blueprint $table) {
             $table->id();
-            $table->text('name')->unique();
+            $table->string('name',100)->unique();
             $table->timestamps();
         });
     }
