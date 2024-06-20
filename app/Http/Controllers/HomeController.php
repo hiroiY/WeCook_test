b3f3118fb2dda8685a31fc7ddd7a01b9cf624e2f
hiroiY <150808041+hiroiY@users.noreply.github.com>
2024-06-20 16:14:17 +0900

    add all of foles

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('homepage.homepage');
+    }
+
+    public function admin()
+    {
+        return view('admin');
+    }
+    public function mypage()
+    {
+        return view('mypage');
+    }
+    public function mypage2()
+    {
+        return view('mybookmark');
+    }
+    public function myrecipe()
+    {
+        return view('mypage.myrecipe');
+    }
+
+
+
+    public function search() 
+    {
+        return view('search.search');
+    }
+    public function profile_edit()
+    {
+        return view('profile_edit');
+    }
+}
