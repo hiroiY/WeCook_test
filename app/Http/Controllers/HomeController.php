d46b3a5e1ef62578361f596d55ef95f5adf7a621
YukoH <hrikerdisk@gmail.com>
2024-06-21 01:58:32 +0900

    [Backend/homepage/homepage] Made forelse roop and Change recipe card's Scss
    Made forelse roop in homepage/recently.blade.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Post; 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -11,18 +14,39 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
+
+     private $post;
+     private $user;
+
+    public function __construct(Post $post, User $user)    
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
+        $this->post = $post;
+        $this->user = $user;
     }
     /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
+    private function getRecentlyPosts()
+    {
+        $home_posts = $this->post->get();
+        $recently_posts = [] ;
+
+        foreach($home_posts as $post)
+        {
+            $recently_posts[] = $post;
+        }
+
+        return  $recently_posts;
+    }
+
     public function index()
     {
-        return view('homepage.homepage');
+        $recently_posts = $this->getRecentlyPosts();
+        return view('homepage.homepage')
+                ->with('recently_posts', $recently_posts);
     }
 
     public function admin()
