b0eb9e2401268529881464e5a3fd36b06e0616bf
YukoH <hrikerdisk@gmail.com>
2024-06-24 19:16:24 +0900

    [Backend/homepage/appetizer] These files were already created on [Backend/homepage/homepage] branch.

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
-
+    const ADMIN_ROLE_ID = 1; //admin user
+    const USER_ROLE_ID = 2; //regular user
     /**
      * The attributes that are mass assignable.
      *
@@ -43,5 +45,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class)->latest();
     }
 }
