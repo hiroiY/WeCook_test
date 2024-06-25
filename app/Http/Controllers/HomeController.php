1b047a2b6d06fe2a844dc13993d952ad0da02beb
YukoH <hrikerdisk@gmail.com>
2024-06-25 21:16:36 +0900

    [Backend/homepage/side_dish] I  created the side dish page on the homepage.
    added :
    - the function getSidedishPosts() in "HomeController"
    - New route in "web.php"
       /home/sidedish'
       /home/recently' -> Because  I cannot see the recently shared page I added that route.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -46,6 +46,8 @@ class HomeController extends Controller
         // Create LengthAwarePaginator instance 
         $recentlyPaginatedItems = new LengthAwarePaginator( $recentlyPageItems, count($home_posts),$perPage, $page,['path' => LengthAwarePaginator::resolveCurrentPath(),]);
 
+        $recentlyPaginatedItems->withPath('/home/recently');
+
         return $recentlyPaginatedItems;
     }
 
@@ -68,14 +70,37 @@ class HomeController extends Controller
         return $appetizerPaginatedItems;
     }
 
+    private function getSidedishPosts($page = 1, $perPage = 9, $dishSide = 2)
+    {
+        //Get the latest posts which dish_id is 2
+       $get_dish_id_2 = $this->post->where('dish_id',$dishSide)->latest();
+
+       //Get 30 posts from last
+       $sideDishPosts = $get_dish_id_2->take(30)->get();
+
+       //Slices the items displayed on the side dish page.
+        $sideDishItems = $sideDishPosts->slice(($page - 1) * $perPage, $perPage)->all();
+
+        //Create LengthAwarePaginator instance 
+       $sideDishPaginatedItems = new LengthAwarePaginator($sideDishItems,count($sideDishPosts), $perPage, $page, ['path' => LengthAwarePaginator::resolveCurrentPath()]);
+
+       $sideDishPaginatedItems->withPath('/home/sidedish');
+
+        return $sideDishPaginatedItems;
+    }
+
     public function index(Request $request)
     {
         $page = $request->input('page', 1);
+        //dish_id
         $dishAppetizer = 1;
+        $dishSide = 2;
+
         $recently_posts = $this->getRecentlyPosts($page);
         $appetizer_posts = $this->getAppetizerPosts($page,9,$dishAppetizer);
+        $sideDish_posts =  $this->getSidedishPosts($page, 9, $dishSide);
 
-        return view('homepage.homepage', compact('recently_posts','appetizer_posts'));
+        return view('homepage.homepage', compact('recently_posts','appetizer_posts','sideDish_posts'));
     }
     public function admin()
     {
