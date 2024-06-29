8b1e27b357efa3da6fd33bccb2862672e363ffa8
FuShiba <fu.disney@gmail.com>
2024-06-29 17:07:09 +0900

    [Backednd/admin/postmanagement] Created -search_post.blade -comments_table -Comment model
    Edited
    -footer, navbar background color change
    -usermanagement.blade
    -postmanagement.blade
    -web.php
    -AdminContorller
    -admin.scss
    -user_status
    -post_status
    -Post.php
    
    Added
    -deleted_at col to the comments table

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers;
 use App\Models\User;
+use App\Models\Post;
+use App\Models\Dish;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 use Illuminate\Pagination\LengthAwarePaginator;
@@ -12,9 +14,10 @@ class AdminController extends Controller
 {
     protected $user;
 
-    public function __construct(User $user)
+    public function __construct(User $user, Post $post)
     {
         $this->user = $user;
+        $this->post = $post;
     }
 
     public function search_username(Request $request){
@@ -25,6 +28,15 @@ class AdminController extends Controller
         ->paginate(10);
         return view ('admin.search_user', compact('searched_username')) ;
     }
+    public function search_post(Request $request){
+        $search_post = $request->input('search_post');
+        $searched_post = Post::where('title', 'like', '%'. $search_post . '%')
+        ->withTrashed()
+        ->latest()
+        ->paginate(10);
+        return view ('admin.search_post', compact('searched_post')) ;
+    }
+
 
     public function index(Request $request)
     {
@@ -37,22 +49,40 @@ class AdminController extends Controller
         return redirect()->back();
     }
 
-    public function activate(Request $request, $id)
+    public function activate($id)
     {
         $this->user->onlyTrashed()->findOrFail($id)->restore();
         return redirect()->back();
     }
+    public function postmanagement()
+    {
+        $all_posts = Post::withTrashed()->latest()->paginate(10);
+        return view('admin.postmanagement', compact('all_posts'));
+    }
+    public function activatePost($id)
+    {
+        $this->post->onlyTrashed()->findOrFail($id)->restore();
+        return redirect()->back();
+    }
+
+    public function deactivatePost(Request $request, $id)
+    {
+        $this->post->destroy($id);
+        return redirect()->back();
+}
+
+
 
 
 
     // public function postmanagement(){
     //     return view('admin.postmanagement');
     // }
-    // public function userstatus(){                                 
-    //     return view('admin.modal.user_status');                  
-    // }                                                             
-    // public function poststatus(){                                 
-    //     return view('admin.modal.post_status');                 
-    // }
+    public function userstatus(){                                 
+        return view('admin.modal.user_status');                  
+    }                                                             
+    public function poststatus(){                                 
+        return view('admin.modal.post_status');                 
+    }
 
 }
