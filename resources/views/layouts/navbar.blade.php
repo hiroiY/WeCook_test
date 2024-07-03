b450f181b4dfda2d5b0d7124c976761fa3b0124c
YukoH <hrikerdisk@gmail.com>
2024-07-03 21:13:21 +0900

    [Backend/tab_action] I discussed it with my batch member. He uses those when creating the logout feature. But if it is on the navbar's blade, the user cannot use the navbar's feature. He agrees it's deleted on there. Because it's still running when it was commented out.

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,10 +1,6 @@
 @vite(['resources/sass/navbar.scss'])
 @vite(['resources/js/logout_modal.js'])
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
- integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> <!-- has popper already -->
 <nav class="navbar navbar-expand-md shadow-sm p-1">
   <div class="container">
     <a 
