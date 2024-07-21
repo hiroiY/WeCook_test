faf317545600526226e0bee152e5fa820c239c2d
YukoH <hrikerdisk@gmail.com>
2024-07-22 01:56:25 +0900

    [Backend/bugsFixs2]I added /#comment-start at the comment link in all of the recipe cards.
     It can transit to the comment area.

--- resources/views/homepage/appetizer.blade.php
+++ resources/views/homepage/appetizer.blade.php
@@ -97,7 +97,7 @@
                     <li  style="width: auto; margin-right: 1em; padding-right:0;">
                       <a 
                         class="dropdown-item tog-icon" 
-                        href="{{ route('detailrecipe', ['post_id'=>$post->id, 'user_id'=>$post->user->id]) }}"
+                        href="{{ route('detailrecipe', ['post_id'=>$post->id, 'user_id'=>$post->user->id]) }}/#comment-start"
                       >
                         &#xf086; 
                         <span>{{ $post->comment->count() }}</span>
