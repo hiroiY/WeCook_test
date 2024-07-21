8266074776a782f3a3b419284e698b2f06269fff
YukoH <hrikerdisk@gmail.com>
2024-07-22 03:07:43 +0900

    [Baskend/Q&A] I created Q&A's migration files.
    -questions table
    -answers table
    -question_answer table

---
+++ database/migrations/2024_07_21_173457_create_answers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignID('post_id')->constrained()->onDelete('cascade');
+            $table->foreignID('user_id')->constrained()->onDelete('cascade');
+            $table->string('body',300);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('answers');
+    }
+};
