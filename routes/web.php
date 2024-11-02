5570c034b7a8f1185d069e8745a79f3f1f4ddedc
YukoH <hrikerdisk@gmail.com>
2024-11-02 14:36:56 +0900

    resolve web.php conflicts

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Auth;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Auth::routes();
-// require __DIR__ . '/auth.php';
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-// Logout modal
-// Route::get('/logoutmodal', [HomeController::class, 'logoutmodal']);
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/mypage/profile_edit', [App\Http\Controllers\HomeController::class, 'profile_edit'])->name('profile_edit');
-Route::get('/usermanagement', [App\Http\Controllers\AdminController::class, 'usermanagement'])->name('usermanagement');
-Route::get('/postmanagement', [App\Http\Controllers\AdminController::class, 'postmanagement'])->name('postmanagement');
-Route::get('/user-status', [App\Http\Controllers\AdminController::class, 'userstatus'])->name('userstatus');
-Route::get('/post-status', [App\Http\Controllers\AdminController::class, 'poststatus'])->name('poststatus');
-Route::get('/mypage/myrecipe', [App\Http\Controllers\HomeController::class, 'myrecipe'])->name('myrecipe');
-Route::get('/mypage/mybookmark', [App\Http\Controllers\HomeController::class, 'mypage2'])->name('mybookmark');
-Route::get('/editmyrecipe', [App\Http\Controllers\RecipeController::class, 'editmyrecipe'])->name('editmyrecipe');
-Route::get('/delete-recipe', [App\Http\Controllers\RecipeController::class, 'deleterecipe'])->name('deleterecipe');
-Route::get('/recipe/writer',[UserController::class, 'writer'])->name('writer');
-Route::get('/search',[HomeController::class, 'search'])->name('search');
-
-Route::get('/createrecipe',[App\Http\Controllers\RecipeController::class,'createrecipe']);
-
-Route::get('/detailrecipe',[App\Http\Controllers\RecipeController::class,'detailrecipe']);
-=======
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
@@ -153,4 +120,3 @@ Route::post('/recipe/{question_id}/answer', [AnswerController::class, 'storeAnsw
 Route::patch('/recipe/{post_id}/answer/update', [AnswerController::class, 'updateAnswer'])->name('update.answer');
 Route::delete('/recipe/{post_id}/answer/delete', [AnswerController::class, 'deleteAnswer'])->name('delete.answer');
 
->>>>>>> 77b54b46424960bdd2cf7ccfb3a2614090b111f9
